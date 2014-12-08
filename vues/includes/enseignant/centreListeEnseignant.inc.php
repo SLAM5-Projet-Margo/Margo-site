59376d7d8c9057eb83faa541cefae229359de511
btssio <btssio@btssio-VirtualBox>
2014-12-08 17:46:32 +0100

    v9 - Début de fonctionnalité ajout et suppression des fonctions (classe, eleves, enseignants, filieres)

--- vues/includes/enseignant/centreListeEnseignant.inc.php
+++ vues/includes/enseignant/centreListeEnseignant.inc.php
@@ -3,13 +3,15 @@
 $ListeEnseignant = $this->lireDonnee('listeEnseignant');
 echo "<div id='titreSection'><h1>".$this->lireDonnee('titreSection')."</h1></div>";
 ?>
-    <fieldset id="fieldsetTab">
+    <fieldset id="fieldsetTab">    
+        <a href="?controleur=enseignant&action=afficherLesEnseignants"><img src="../vues/images/add-list-32.png" style="width:32px; height:32px;" alt="" /></a>
         <legend>Liste des Enseignants</legend>
         <table id ="tab" border='1'>
             <tr>
                 <th>Nom</th>
                 <th>Prenom</th>
                 <th>Mail</th>
+                <th>Supprimer</th>
             </tr>
             <?php
                 foreach ($ListeEnseignant as $unEnseignant){
@@ -17,6 +19,7 @@ echo "<div id='titreSection'><h1>".$this->lireDonnee('titreSection')."</h1></div
                             . '<td>'.$unEnseignant->getNom().'</td>'
                             . '<td>'.$unEnseignant->getPrenom().'</td>'
                             . '<td>'.$unEnseignant->getMail().'</td>'
+                            . '<td><a href="?controleur=enseignant&action=afficherLesEnseignants"><img src="../vues/images/error-6-32.png" style="width:32px; height:32px;" alt="" /></a></td>'
                         .'</tr>';
                 }
             ?>
